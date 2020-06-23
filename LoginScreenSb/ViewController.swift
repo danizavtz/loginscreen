82e3ce68f7aa98360b162f889990eff94048b4e5
Danizavtz <daninove@gmail.com>
2020-06-23 12:59:58 -0300

    Initial Commit

---
+++ LoginScreenSb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginScreenSb
+//
+//  Created by Daniel Lucena Pires on 23/06/20.
+//  Copyright © 2020 Pessoal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
