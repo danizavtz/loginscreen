2c0b37fad43ce05bb8dbbbfe8388c7b2b25c9ee8
Danizavtz <daninove@gmail.com>
2020-07-05 01:30:22 -0300

    Inserçao de rotina para fazer login no aplicativo, usando puramente swift

---
+++ LoginScreenSb/models/AuthenticationTokenData.swift
@@ -0,0 +1,13 @@
+//
+//  AuthenticationTokenData.swift
+//  LoginScreenSb
+//
+//  Created by Daniel Lucena Pires on 05/07/20.
+//  Copyright © 2020 Pessoal. All rights reserved.
+//
+
+import Foundation
+
+struct AuthenticationToken : Codable {
+    let token : String
+}
