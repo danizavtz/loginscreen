8b884808ba8608117ee77bc516cceb47cb077884
Danizavtz <daninove@gmail.com>
2020-07-05 20:01:04 -0300

    Insercao de tabbar

---
+++ LoginScreenSb/ViewControllers/Configuracao.swift
@@ -0,0 +1,9 @@
+//
+//  Configuracao.swift
+//  LoginScreenSb
+//
+//  Created by Daniel Lucena Pires on 05/07/20.
+//  Copyright © 2020 Pessoal. All rights reserved.
+//
+
+import Foundation
---
+++ LoginScreenSb/ViewControllers/ConfiguracaoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ConfiguracaoViewController.swift
+//  LoginScreenSb
+//
+//  Created by Daniel Lucena Pires on 05/07/20.
+//  Copyright © 2020 Pessoal. All rights reserved.
+//
+
+import UIKit
+
+class ConfiguracaoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
