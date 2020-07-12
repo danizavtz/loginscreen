e714986867ab974ed2b35c7f68e3f0f52b512377
Danizavtz <daninove@gmail.com>
2020-07-12 07:52:50 -0300

    Insercao de tela de cadastro

---
+++ LoginScreenSb/models/Cadastro.swift
@@ -0,0 +1,15 @@
+//
+//  Cadastro.swift
+//  LoginScreenSb
+//
+//  Created by Daniel Lucena Pires on 12/07/20.
+//  Copyright © 2020 Pessoal. All rights reserved.
+//
+
+import Foundation
+
+struct Cadastro : Codable {
+    let username: String
+    let senha: String
+    let email: String
+}
